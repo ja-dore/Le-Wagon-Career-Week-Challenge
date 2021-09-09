eb824e865bae4fcc5710293137caa741c6cac82f
Dorien Reynders <dorien.reynders@hotmail.com>
2021-09-09 14:16:38 +0200

    finished API for mercedes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
+require 'json'
+require 'open-uri'
+
 class PagesController < ApplicationController
-  def home
+
+  def museum
+    # receives the data by #home by a click on the map
+    # the url for the mapbox API respons
+    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?proximity=#{params[:lng]},#{params[:lat]}&limit=7&access_token=#{ENV['MAPBOX_API_KEY']}"
+    input_mapbox = JSON.parse(URI.open(url).read)
+
+    # restore the input in a new output (sorted by postcode and name)
+    output = {}
+    input_mapbox["features"].each do |museum|
+      postcode = museum["context"][0]["text"]
+      name = museum["text"]
+      output.include?(postcode) ? output[postcode] << name : output[postcode] = [name]
+    end
+    # return the new output towards the view and converted into a JSON
+    @output = output.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/museums', to: 'pages#museum' # to this page our new API must respond
 end
